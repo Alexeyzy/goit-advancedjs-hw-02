d1ceee184aec42ab1e2a2fe12e4b12b547236c66
Alexey <a03221983@gmail.com>
2025-11-02 13:50:22 +0200

    2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,31 +2,26 @@ import iziToast from 'izitoast';
 
 const form = document.querySelector('.form');
 
-const delay = 1000;
-
 form.addEventListener('submit', event => {
   event.preventDefault();
   
-  // const fulfilled = form.elements.email.value.trim();
-  // const rejected = form.elements.message.value.trim();
+  const delay = form.elements.delay.value;
+  const promise = new Promise((resolve, reject) => {
 
-// if(){
-
-// }else{
-
-// }
-
-  iziToast.show({ 
-    message: `✅ Fulfilled promise in ${delay}ms`,
-    color: 'green', 
-    position: 'topRight',  
-  });
- 
-  iziToast.show({
-    message: `❌ Rejected promise in ${delay}ms`,
-    color: 'green', 
-    position: 'topRight',  
-  });
-console.dir(form);
-console.dir(event);
+    setTimeout(() => {
+      if(form.elements.state.value === "fulfilled"){
+        resolve(iziToast.success({ 
+          message: `Fulfilled promise in ${delay}ms`,
+          color: 'green', 
+          position: 'topRight',  
+        }));
+      }else{
+        reject(iziToast.error({
+          message: `Rejected promise in ${delay}ms`,
+          color: 'green', 
+          position: 'topRight',  
+        }));
+      }
+    }, delay)
+  })
 });
